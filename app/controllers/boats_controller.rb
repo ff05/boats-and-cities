dc21d4642b39456040a9ed87acc4311eb1e70611
2Deo <ziemowit.holda@gmail.com>
2017-10-07 15:08:07 +0200

    Boats controller update create edit save delete

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -2,4 +2,50 @@ class BoatsController < ApplicationController
   def show
     @boat = Boat.find(params[:id])
   end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+
+    if @boat.save
+      redirect_to @boat
+    else
+      render "new"
+    end
+end
+
+def edit
+  @boat = Boat.find(params[:id])
+end
+
+def update
+  @boat = Boat.find(params[:id])
+
+  if @boat.update_attributes(boat_params)
+    redirect_to @boat
+  else
+    render "edit"
+  end
+
+end
+
+def destroy
+  @boat = Boat.find(params[:id])
+
+  @boat.destroy
+
+  redirect_to boat_path
+
+end
+
+private
+
+def boat_params
+  params.require(:boat).permit(:name, :seats, :price, :image_url)
+end
+
+
 end
