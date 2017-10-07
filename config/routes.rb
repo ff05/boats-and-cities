50bf3ab87832a07387ac4aa27039b1656ea027cf
2Deo <ziemowit.holda@gmail.com>
2017-10-07 13:51:04 +0200

    Routes creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'cities#index'
+
+get 'boats' => 'boats#index'
+get 'cities' => 'cities#index'
+
+get 'boats/new' => 'boats#new', as: :new_boat
+get 'boats/:id' => 'boats#show', as: :boat
+get 'cities/:id' => 'cities#show' as: :city
+
+post 'boats' => 'boats#create'
+get 'boats/:id/edit' => 'boats#edit', as: :edit_boat
+
+patch 'boats/:id' => 'boats#update'
+delete 'boats/:id' => 'boats#destroy'
+
+
 end
